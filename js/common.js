4ca1b73034a2c4966a2460685e4cb6792a699756
jayv <qkr981014@naver.com>
2025-09-04 15:57:06 +0900

    최종 완성

---
+++ js/common.js
@@ -0,0 +1,17 @@
+// .btn-menu 클릭 시 .m-menu에 active 클래스 부여
+// .btn-close 클릭 시 .m-menu에 active 클래스 삭제
+document.addEventListener("DOMContentLoaded", function () {
+  var btnMenu = document.querySelector(".btn-menu");
+  var btnClose = document.querySelector(".btn-close");
+  var mMenu = document.querySelector(".m-menu");
+  if (btnMenu && mMenu) {
+    btnMenu.addEventListener("click", function () {
+      mMenu.classList.add("active");
+    });
+  }
+  if (btnClose && mMenu) {
+    btnClose.addEventListener("click", function () {
+      mMenu.classList.remove("active");
+    });
+  }
+});
